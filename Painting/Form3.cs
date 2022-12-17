e81842deb6bcbe3ff8ce267e905d57761d9fca3a
warhogz <90264787+warhogz@users.noreply.github.com>
2022-12-17 05:06:16 +0300

    Add files via upload

---
+++ Painting/Form3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+namespace WindowsFormsKhasaev
+{
+    public partial class Form3 : Form
+    {
+        int x, y, h, w;
+        Pen p =new Pen(Color.Red, 3); 
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button1.BackColor = colorDialog1.Color;
+            p.Color = colorDialog1.Color;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
+
+        private void Form3_MouseUp(object sender, MouseEventArgs e)
+        {
+            h = e.X - x;
+            w = e.Y - y;
+            Graphics g = this.CreateGraphics ();
+            Rectangle shape = new Rectangle(x, y, w, h);
+            if (radioButton1.Checked)
+            {
+                g.DrawEllipse(p, shape);
+            }
+            else if (radioButton2.Checked)
+            {
+                g.DrawRectangle(p, shape);
+            }
+        }
+    }
+}
